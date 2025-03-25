d30fad3b07c0676090a47aec95db1e3e34681c45
ACT-STUDENT <ACT-STUDENT@LAB4-PC43>
2025-03-25 09:30:00 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Odato_UserManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

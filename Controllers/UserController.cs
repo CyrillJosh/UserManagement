10d66a2d8403c6cdb72e8b099f868e86b4311673
cy <act.cjodato@gmail.com>
2025-03-25 09:52:06 +0800

    Added Index and Create views

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,7 +4,22 @@ namespace Odato_UserManagement.Controllers
 {
     public class UserController : Controller
     {
+        //Fields
+        private readonly MyDBContext _context;
+
+        //Constructor
+        public UserController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        //Index
         public IActionResult Index()
+        {
+            return View();
+        }
+        //Create
+        public IActionResult Create()
         {
             return View();
         }
